81ba8f66fa35aa9aab5231e25a2c819c06e56df5
Negin Soleiamni <negin.kdk1@gmail.com>
2022-08-07 22:29:00 +0430

    Header & Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close")
 
+/*===== MENU SHOW =====*/
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll(".nav__link")
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu")
+    navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction))
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
@@ -10,7 +35,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
