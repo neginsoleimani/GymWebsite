1ed21674e7e01b7cd7ca902a7d61c8291f1df894
Negin Soleiamni <negin.kdk1@gmail.com>
2022-08-10 03:09:54 +0430

    Email js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,6 @@ const linkAction = () => {
 
 navLink.forEach(n => n.addEventListener("click", linkAction))
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
     const header = document.getElementById("header")
@@ -39,7 +38,6 @@ const scrollHeader = () => {
     }
 }
 window.addEventListener("scroll", scrollHeader)
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
@@ -105,7 +103,50 @@ const calculateBmi = (e) => {
 
 calculateForm.addEventListener("submit", calculateBmi)
 
-
-
-
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message'),
+    contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // Check if the field has a value
+    if (contactUser.value === '') {
+        // Add and remove color
+        contactMessage.classList.remove("color-green")
+        contactMessage.classList.add("color-red")
+
+        // Show message
+        contactMessage.textContent = 'You must enter your email'
+
+        // Remove message three seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 3000)
+    } else {
+
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('', '', '', '')
+            .then(() => {
+                // Show message and add color
+                contactMessage.classList.add("color-green")
+                contactMessage.textContent = 'You registered successfully'
+
+                // Remove message after three seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 3000)
+
+            }, (error) => {
+                // Mail sending error
+                alert('OOPS! SOMETHING HAS FAILED...',error)
+            }
+            )
+
+            // To clear the input field
+            contactUser.value=''
+    }
+}
+
+contactForm.addEventListener("submit", sendEmail)
