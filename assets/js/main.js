5b38b0fac000f29bc59a7f8669ecfd434f7edf50
Negin Soleiamni <negin.kdk1@gmail.com>
2022-08-10 00:08:04 +0430

    Calculate BMI

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,11 +60,51 @@ const calculateBmi = (e) => {
     e.preventDefault()
 
     // Check if the fields have a value
-    
+    if (calculateCm.value === '' || calculateKg === '') {
+        // Add and remove color
+        calculateMessage.classList.remove("color-green")
+        calculateMessage.classList.add("color-red")
+
+        // Show message
+        calculateMessage.textContent = 'Fill in the Height and Weight'
+
+        // Remove message three seconds
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else {
+        // BMI Formula
+        const cm = calculateCm.value / 100,
+            kg = calculateKg.value,
+            bmi = Math.round(kg / (cm * cm))
+
+
+        // Show your health status
+        if (bmi < 18.5) {
+            // Add color and display message
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny`
+        } else if (bmi < 25) {
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} you are healthy`
+        } else {
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} you are overweight`
+        }
+
+        // To clear the input field
+        calculateCm.value=''
+        calculateKg.value=''
+
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 4000)
+        
+    }
 
 }
 
-calculateForm.addEventListener("submit",calculateBmi)
+calculateForm.addEventListener("submit", calculateBmi)
 
 
 
