cc5b7cd916440e55ff534e0a24968603f9900035
Negin Soleiamni <negin.kdk1@gmail.com>
2022-08-12 23:34:56 +0430

    scroll reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,21 @@ window.addEventListener("scroll", scrollUp)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+})
+
+sr.reveal(`.home__data , .footer__container , .footer__group`)
+sr.reveal(`.home__img`,{delay:700,origin:'botton'})
+sr.reveal(`.logos__img , .program__card , .pricing__card`,{interval:100})
+sr.reveal(`.choose__img , .calculate__content`,{origin:'left'})
+sr.reveal(`.choose__content,.calculate__img`,{origin:'right'})
+
+
+
 
 
 /*=============== CALCULATE JS ===============*/
