e474e4e9b779745bc72324ae751c95814a1341c8
Negin Soleiamni <negin.kdk1@gmail.com>
2022-08-12 20:49:26 +0430

    Scroll section active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,26 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id")
+
+        if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+
+    })
+}
+window.addEventListener("scroll", scrollActive)
+
 
 
 /*=============== SHOW SCROLL UP ===============*/
